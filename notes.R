ffc46febc4301def907bb3d54eea81ffccc8b00c
“Zarinah <zarinahagnew@gmail.com>
2017-03-06 15:56:29 -0800

    prepping for last exercize

--- notes.R
+++ notes.R
@@ -14,11 +14,37 @@ plot(gapM)
 plot(gapM$pop)
 
 plot(gapM$pop, gapM$lifeExp)
-plot(x=gapM$pop, y=gapM$lifeExp, main = "this is the title", xlab = "popn", ylab = "life exp")
+plot(x=gapM$pop, y=gapM$lifeExp, main = "gapM", xlab = "popn", ylab = "life exp")
 
 # subset the data: trying to see populations over 10,000 people in population
 # gapM$pop >=10000
+gapM$pop >=10000
+gapMlargepop <- gapM[gapM$pop >=100000000,]
+plot(x=gapMlargepop$pop, y=gapMlargepop$lifeExp, main = "gapMlargepop", xlab = "popn", ylab = "life exp")
 
-
-
+# take subset of the data
+?subset
 testdata=read.csv(file = "data/testdata.csv", sep = "\t", header = TRUE)
+
+
+# pivot tables
+agg_data <- aggregate(.~country, data=gapM, FUN = mean) # . is short hand for 'everything
+View(agg_data)
+
+agg_data_country <- aggregate(pop + lifeExp~country, data=gapM, FUN = mean) # . is short hand for 'everything
+
+# cbind and rbind (coloum and row bind)
+agg_data_country_b <- aggregate(cbind(pop,lifeExp)~country, data=gapM, FUN = mean) # . is short hand for 'everything
+View(agg_data_country)
+
+plot(agg_data_country$year)
+
+##
+zdata=read.csv(file = "output/testdata.txt")
+View(zdata)
+
+z <- read.csv(file = "output/testdataz.csv")
+
+filez <- read.csv(file = "output/testdataz.csv", header=TRUE)
+
+
